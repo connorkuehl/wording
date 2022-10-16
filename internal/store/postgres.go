efac806294442fd136e5eb1f89cf5eafebf23431
Connor Kuehl <cipkuehl@gmail.com>
2022-10-16 14:30:08 -0500

    add modified_at, accessed_at attributes to games

--- internal/store/postgres.go
+++ internal/store/postgres.go
@@ -72,10 +72,16 @@ func (s *PostgresStore) CreateGame(ctx context.Context, adminToken, token, answe
 }
 
 func (s *PostgresStore) Game(ctx context.Context, adminToken string) (*wording.Game, error) {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
 	query := `SELECT token, answer, guess_limit FROM games WHERE admin_token = $1`
 
 	game := wording.Game{AdminToken: adminToken}
-	err := s.db.QueryRowContext(ctx, query, adminToken).
+	err = tx.QueryRowContext(ctx, query, adminToken).
 		Scan(&game.Token, &game.Answer, &game.GuessLimit)
 	if errors.Is(err, sql.ErrNoRows) {
 		err = ErrNotFound
@@ -84,16 +90,27 @@ func (s *PostgresStore) Game(ctx context.Context, adminToken string) (*wording.G
 		return nil, err
 	}
 
-	return &game, nil
+	_, err = tx.ExecContext(ctx, `UPDATE games SET accessed_at = NOW() WHERE admin_token = $1`, adminToken)
+	if err != nil {
+		return nil, err
+	}
+
+	return &game, tx.Commit()
 }
 
 func (s *PostgresStore) GameByToken(ctx context.Context, token string) (*wording.Game, error) {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
 	query := `SELECT admin_token, answer, guess_limit FROM games WHERE token = $1`
 
 	game := wording.Game{
 		Token: token,
 	}
-	err := s.db.QueryRowContext(ctx, query, token).
+	err = tx.QueryRowContext(ctx, query, token).
 		Scan(&game.AdminToken, &game.Answer, &game.GuessLimit)
 	if errors.Is(err, sql.ErrNoRows) {
 		err = ErrNotFound
@@ -102,22 +119,44 @@ func (s *PostgresStore) GameByToken(ctx context.Context, token string) (*wording
 		return nil, err
 	}
 
-	return &game, nil
+	_, err = tx.ExecContext(ctx, `UPDATE games SET accessed_at = NOW() WHERE token = $1`, token)
+	if err != nil {
+		return nil, err
+	}
+
+	return &game, tx.Commit()
 }
 
 func (s *PostgresStore) Plays(ctx context.Context, gameToken, playerToken string) (*wording.Plays, error) {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
 	plays := &wording.Plays{}
 
 	query := `SELECT guesses FROM attempts WHERE game_token=$1 AND player_token=$2`
-	err := s.db.QueryRowContext(ctx, query, gameToken, playerToken).Scan(pq.Array(&plays.Attempts))
+	err = tx.QueryRowContext(ctx, query, gameToken, playerToken).Scan(pq.Array(&plays.Attempts))
 	if errors.Is(err, sql.ErrNoRows) {
 		return nil, ErrNotFound
 	}
 
-	return plays, nil
+	_, err = tx.ExecContext(ctx, `UPDATE games SET accessed_at = NOW() WHERE token = $1`, gameToken)
+	if err != nil {
+		return nil, err
+	}
+
+	return plays, tx.Commit()
 }
 
 func (s *PostgresStore) PutPlays(ctx context.Context, gameToken, playerToken string, plays *wording.Plays) error {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
 	query := `INSERT INTO attempts (
 		game_token,
 		player_token,
@@ -129,8 +168,17 @@ func (s *PostgresStore) PutPlays(ctx context.Context, gameToken, playerToken str
 	) ON CONFLICT (game_token, player_token) DO UPDATE SET guesses = $3`
 	args := []any{gameToken, playerToken, pq.Array(plays.Attempts)}
 
-	_, err := s.db.ExecContext(ctx, query, args...)
-	return err
+	_, err = tx.ExecContext(ctx, query, args...)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.ExecContext(ctx, `UPDATE games SET accessed_at = NOW(), modified_at = NOW() WHERE token = $1`, gameToken)
+	if err != nil {
+		return err
+	}
+
+	return tx.Commit()
 }
 
 func (s *PostgresStore) IncrementStats(ctx context.Context, stats wording.IncrementStats) error {
--- migrations/000001_create_games_table.up.sql
+++ migrations/000001_create_games_table.up.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS games (
     admin_token TEXT PRIMARY KEY,
     token TEXT,
     created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    accessed_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    modified_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
     answer TEXT NOT NULL,
     guess_limit INTEGER NOT NULL
 );
