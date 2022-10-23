7e6ec9fb0eca3208aa4cdfe0005fd18de70a0b7d
Connor Kuehl <cipkuehl@gmail.com>
2022-10-23 15:48:03 -0500

    document generator

---
+++ internal/generator/doc.go
@@ -0,0 +1,2 @@
+// Package generator provides URL slug (game token) generators.
+package generator
--- internal/generator/uuid.go
+++ internal/generator/uuid.go
@@ -1,13 +1,18 @@
 package generator
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+)
 
+// UUIDGenerator generates UUIDs.
 type UUIDGenerator struct{}
 
+// NewUUIDGenerator creates a new UUIDGenerator.
 func NewUUIDGenerator() *UUIDGenerator {
 	return new(UUIDGenerator)
 }
 
+// NewToken creates a UUID token.
 func (g *UUIDGenerator) NewToken() string {
 	return uuid.NewString()
 }
