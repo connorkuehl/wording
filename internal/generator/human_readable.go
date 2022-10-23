2df155b76576c610a63614483c679e06a9d279e4
Connor Kuehl <cipkuehl@gmail.com>
2022-10-23 15:11:41 -0500

    remove dead code

--- internal/generator/human_readable.go
+++ internal/generator/human_readable.go
@@ -13,58 +13,6 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-var builtinNouns = []string{
-	"tornado",
-	"mahogany",
-	"boldness",
-	"fan",
-	"majority",
-	"panda",
-	"dragon",
-}
-
-var builtinAdjectives = []string{
-	"criminal",
-	"adoring",
-	"vacant",
-	"mysterious",
-	"frightful",
-	"hungry",
-	"sad",
-}
-
-var builtinColors = []string{
-	"violet",
-	"indigo",
-	"blue",
-	"green",
-	"yellow",
-	"orange",
-	"red",
-}
-
-type RandInt func() int
-
-type HumanReadable struct {
-	randInt RandInt
-}
-
-func NewHumanReadableGenerator(g RandInt) *HumanReadable {
-	return &HumanReadable{
-		randInt: g,
-	}
-}
-
-func (g *HumanReadable) NewToken() string {
-	parts := []string{
-		builtinColors[g.randInt()%len(builtinColors)],
-		builtinAdjectives[g.randInt()%len(builtinAdjectives)],
-		builtinNouns[g.randInt()%len(builtinNouns)],
-	}
-
-	return strings.Join(parts, "-")
-}
-
 type RandomWordClient struct {
 	baseURL  string
 	client   http.Client
